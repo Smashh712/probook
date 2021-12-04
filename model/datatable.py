ba832674982772b1b681858d625941eea2c81423
Altaaaaaaa <jeewon9901@naver.com>
2021-12-04 21:48:59 +0900

    code

--- model/datatable.py
+++ model/datatable.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
-f = open('../data/booklist.txt','r')
+f = open('../data/booklist.txt','r', encoding='UTF-8')
 while True:
     line = f.readline()
     if not line : break
 f.close()
+
+user_id, book_id, score = input("사용자 id, 책 id, 평점을 입력해주세요.\n").split(',')
+
+print(user_id)
+print(book_id)
+print(score)
