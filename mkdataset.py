7cf6181b58ce33597630428bfab39d662a2c9a4a
Altaaaaaaa <jeewon9901@naver.com>
2021-12-04 19:34:37 +0900

    code

--- mkdataset.py
+++ mkdataset.py
@@ -3,5 +3,10 @@ import xmltodict
 import json
 
 r = requests.get(f"https://aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttbmlboy101516001&Query=python&QueryType=Keyword&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20070901&Sort=Accuracy")
-        
+
+cc = xmltodict.parse(r.text) # return collections.OrderedDict
+dd = json.loads(json.dumps(cc)) # return dict
+
 print(r)
+print(cc)
+print(dd)
