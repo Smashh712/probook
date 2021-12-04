b9ad94188b4be1a60ad2b45b663c6edb1fb77e87
Altaaaaaaa <jeewon9901@naver.com>
2021-12-04 19:33:12 +0900

    code

--- mkdataset.py
+++ mkdataset.py
@@ -2,12 +2,6 @@ import requests
 import xmltodict
 import json
 
-from scipy import spatial
-
-dataSet1 = [5, 5, -4, 0, 0]
-dataSet2 = [5, 5, 5, 5, 0]
-dataSet3 = [5, 5, 0, 0, 0]
-result = 1 - spatial.distance.cosine(dataSet1, dataSet2)
-result2 = 1 - spatial.distance.cosine(dataSet3, dataSet2)
-
-print(result, result2)
+r = requests.get(f"https://aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttbmlboy101516001&Query=python&QueryType=Keyword&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20070901&Sort=Accuracy")
+        
+print(r)
