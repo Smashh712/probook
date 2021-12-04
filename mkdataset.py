58a3c2355fd21a914bf9c26b401dd08f2c4c2e7e
Altaaaaaaa <jeewon9901@naver.com>
2021-12-04 19:40:21 +0900

    code

--- mkdataset.py
+++ mkdataset.py
@@ -5,15 +5,17 @@ import json
 booklist_id = []
 booklist_name = []
 
-r = requests.get(f"https://aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttbmlboy101516001&Query=python&QueryType=Keyword&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20070901&Sort=Accuracy")
+search_list = ['파이썬', '리엑트', '웹', '프론트엔드', '백엔드', '자바', '자바스크립트']
 
-cc = xmltodict.parse(r.text) # return collections.OrderedDict
-dd = json.loads(json.dumps(cc)) # return dict
+for a in search_list:
+    for i in range(1,6):
+        r = requests.get(f"https://aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=ttbmlboy101516001&Query={a}&QueryType=Keyword&MaxResults=10&start={i}&SearchTarget=Book&output=xml&Version=20070901&Sort=Accuracy")
+        
+        print(a)
 
-print(r)
-print(cc)
-print(dd)
-
-for i in range(len(dd["object"]["item"])):
-    booklist_id.append(dd["object"]["item"][i]["@itemId"])
-    booklist_name.append(dd["object"]["item"][i]["title"])
+        cc = xmltodict.parse(r.text) # return collections.OrderedDict
+        dd = json.loads(json.dumps(cc)) # return dict
+        if "item" in dd["object"]:
+            for i in range(len(dd["object"]["item"])):
+                booklist_id.append(dd["object"]["item"][i]["@itemId"])
+                booklist_name.append(dd["object"]["item"][i]["title"])
