f7364d63595f575e649d4ba51162e386b0e12fe2
Altaaaaaaa <jeewon9901@naver.com>
2021-12-04 19:27:59 +0900

    code

--- mkdataset.py
+++ mkdataset.py
@@ -2,3 +2,12 @@ import requests
 import xmltodict
 import json
 
+from scipy import spatial
+
+dataSet1 = [5, 5, -4, 0, 0]
+dataSet2 = [5, 5, 5, 5, 0]
+dataSet3 = [5, 5, 0, 0, 0]
+result = 1 - spatial.distance.cosine(dataSet1, dataSet2)
+result2 = 1 - spatial.distance.cosine(dataSet3, dataSet2)
+
+print(result, result2)
