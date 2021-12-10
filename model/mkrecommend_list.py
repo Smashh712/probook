191ba436e274c08bc8b08986ad9782d5aa8a858c
Altaaaaaaa <jeewon9901@naver.com>
2021-12-10 22:39:29 +0900

    make recommend code

--- model/mkrecommend_list.py
+++ model/mkrecommend_list.py
@@ -2,3 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import mean_squared_error
+
+score_df = pd.read_csv("../data/user-bookscore.csv", sep=',') #평점 데이터 파일
+
+score_table = pd.pivot_table(score_df, values='score', index=['book_id'], columns=['user_id'], aggfunc=np.sum)
+score_table = score_table.fillna(0)
+
+with open('../data/booklist_id.csv','r', encoding='UTF-8') as f:
+    line = f.read()
+booklist_id = line.split(',')
