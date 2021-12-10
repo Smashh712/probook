39a7dbd46eabb4c2bb0827a95750445d3146b397
Altaaaaaaa <jeewon9901@naver.com>
2021-12-10 22:44:43 +0900

    make recommend code

--- model/mkrecommend_list.py
+++ model/mkrecommend_list.py
@@ -4,8 +4,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 score_df = pd.read_csv("../data/user-bookscore.csv", sep=',') #평점 데이터 파일
 
-score_table = pd.pivot_table(score_df, values='score', index=['book_id'], columns=['user_id'], aggfunc=np.sum)
-score_table = score_table.fillna(0)
+score_tb = pd.pivot_table(score_df, values='score', index=['book_id'], columns=['user_id'], aggfunc=np.sum)
+score_tb = score_tb.fillna(0)
 
 with open('../data/booklist_id.csv','r', encoding='UTF-8') as f:
     line = f.read()
@@ -15,34 +15,34 @@ book_df = pd.DataFrame(index=booklist_id)
 book_df.index.name = 'book_id'
 book_df.columns.name = 'user_id'
 
-usboscore_df = book_df.join(score_table) # 모든 도서 데이터와 합치는 것
-usboscore_df=usboscore_df.fillna(0)
+ub_score_df = book_df.join(score_tb) # 모든 도서 데이터와 합치는 것
+ub_score_df=ub_score_df.fillna(0)
 
-score_table_T = score_table.transpose()
+score_tb_T = score_tb.transpose()
 
-user_sim = cosine_similarity(score_table_T, score_table_T)
+user_sim = cosine_similarity(score_tb_T, score_tb_T)
 
 # cosine_similarity()로 반환된 Numpy 행렬을 도서명으로 매핑해 DataFrame으로 변환
-user_sim_df = pd.DataFrame(data=user_sim, index=score_table.columns,
-                           columns=score_table.columns)
+user_sim_df = pd.DataFrame(data=user_sim, index=score_tb.columns,
+                           columns=score_tb.columns)
 
-know_user = 2021002 # 추천 받고자 하는 user의 id
+rec_user = 2021002 # 추천 받고자 하는 user의 id
 
-rec_list = pd.DataFrame(index=score_table.index)
+rec_list = pd.DataFrame(index=score_tb.index)
 
-for j in score_table.index:
+for j in score_tb.index:
     sum = 0.0
     count = 0
     for i in user_sim_df.index:
-        u = user_sim_df.loc[i, know_user]
-        b = score_table.loc[j, i]
+        u = user_sim_df.loc[i, rec_user]
+        b = score_tb.loc[j, i]
         if b != 0:
             result = u * b
             sum+=result
             count+=1
-    rec_list.loc[j,know_user] = sum / count
+    rec_list.loc[j,rec_user] = sum / count
 
-unread_filter = score_table[know_user] == 0
+unread_filter = score_tb[rec_user] == 0
 rec_list = rec_list.loc[unread_filter]
 
-print(rec_list[know_user].sort_values(ascending=False)) # 최종 결과(추천 리스트)
+print(rec_list[rec_user].sort_values(ascending=False)) # 최종 결과(추천 리스트)
