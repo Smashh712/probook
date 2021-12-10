ef2a980378d87dae16c750d34c9ed258f1392ea1
Altaaaaaaa <jeewon9901@naver.com>
2021-12-11 04:33:24 +0900

    데이터베이스를 생성하기 위한 데이터 수집 코드

--- model/mkdataset.py
+++ model/mkdataset.py
@@ -7,6 +7,10 @@ import csv
 
 booklist_id = []
 booklist_name = []
+booklist_img = []
+booklist_pubdate = []
+booklist_author = []
+booklist_publisher = []
 
 search_list = ['파이썬', '리엑트', '웹', '프론트엔드', '백엔드', '자바', '자바스크립트',
 'C언어', 'C++', 'node.js', '앱 프로그래밍', '자료 구조', '컴퓨터 구조', '알고리즘',
@@ -23,8 +27,23 @@ for a in search_list:
             for i in range(len(dd["object"]["item"])):
                 booklist_id.append(dd["object"]["item"][i]["@itemId"])
                 booklist_name.append(dd["object"]["item"][i]["title"])
+                booklist_img.append(dd["object"]["item"][i]["cover"])
+                booklist_pubdate.append(dd["object"]["item"][i]["pubDate"])
+                booklist_author.append(dd["object"]["item"][i]["author"])
+                booklist_publisher.append(dd["object"]["item"][i]["publisher"])
 
-save_dict = dict(zip(booklist_id, booklist_name))
+def merge_list(*args, fill_value = None):
+    max_length = max([len(lst) for lst in args])
+    merged = []
+    for i in range(max_length):
+        merged.append([
+        args[k][i] if i < len(args[k]) else fill_value for k in range(len(args))
+        ])
+    return merged
+
+book_info = merge_list(booklist_name, booklist_img, booklist_pubdate, booklist_author, booklist_publisher)
+
+save_dict = dict(zip(booklist_id, book_info))
 
 seen = []
 booklist_dict = dict()
@@ -35,15 +54,16 @@ for key, val in save_dict.items():
 
 
 booklist_id = list(booklist_dict)
-booklist_name = list(booklist_dict.values())
+#booklist_name = list(booklist_dict.values())
 
 with open("../data/booklist.txt",'w',encoding='UTF-8') as f:
     for id, name in booklist_dict.items():
         f.write(f'{id} : {name}\n')
 
 with open("../data/bookli.txt",'w',encoding='UTF-8') as f:
-    for id, name in booklist_dict.items():
-        f.write(f'{id}&*&{name}\n')
+    for id, value in booklist_dict.items():
+        list(value)
+        f.write(f'{id}&*&{value[0]}&*&{value[1]}&*&{value[2]}&*&{value[3]}&*&{value[4]}\n')
 
 with open("../data/booklist_id.csv", 'w') as file:
     writer = csv.writer(file)
