9b4af6da27a72030d54fe904ca47393a9d5e7f53
Hyucksoon Jang <smashh712@gmail.com>
2021-12-10 20:45:33 +0900

    오늘의 책

--- django-soft-ui-dashboard/apps/home/views.py
+++ django-soft-ui-dashboard/apps/home/views.py
@@ -6,17 +6,39 @@ Copyright (c) 2019 - present AppSeed.us
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.template import loader
 from django.urls import reverse
 import requests
+import xmltodict
+import random
 
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {"segment": "index"}
-    request
-    html_template = loader.get_template("home/index.html")
-    return HttpResponse(html_template.render(context, request))
+
+    test = requests.get(
+        "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbmlboy101516001&QueryType=ItemNewAll&MaxResults=50&start=1&SearchTarget=Book&CategoryId=437&output=xml&Version=20131101"
+    )
+    xml = xmltodict.parse(test.text)
+
+    new_book = []
+    idx = random.randint(0, len(xml["object"]))
+    title = xml["object"]["item"][idx]["title"]
+    cover = xml["object"]["item"][idx]["cover"]
+    new_book.append([title, cover])
+
+    idx2 = idx
+    while idx2 == idx:
+        idx2 = random.randint(0, len(xml["object"]))
+
+    title = xml["object"]["item"][idx]["title"]
+    cover = xml["object"]["item"][idx]["cover"]
+    new_book.append([title, cover])
+
+    context = {"a": new_book}
+    print(context)
+    return render(request, "home/index.html", context)
 
 
 @login_required(login_url="/login/")
